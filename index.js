02e3742ed8d42e22337320fb81f2108e21ad39f1
Tomer Harari <tharari21@gmail.com>
2022-07-14 14:54:54 -0400

    commit tree 77d9783d9bd3f027951e6f997b372b47f2f43d5a

--- index.js
+++ index.js
@@ -1,1 +1,66 @@
 // Your code here
+const createEmployeeRecord = (employee) => {
+  const [firstName, familyName, title, payPerHour] = employee;
+  return {
+    firstName: firstName,
+    familyName: familyName,
+    title: title,
+    payPerHour: payPerHour,
+    timeInEvents: [],
+    timeOutEvents: [],
+  };
+};
+const createEmployeeRecords = (employees) => {
+  return employees.map(createEmployeeRecord);
+};
+const getDateAndHour = (dateStamp) => {
+  const [date, hour] = dateStamp.split(" ");
+  return [date, parseInt(hour, 10)];
+};
+const createTimeInEvent = (employeeRecord, dateStamp) => {
+  const [date, hour] = getDateAndHour(dateStamp);
+  employeeRecord.timeInEvents.push({
+    type: "TimeIn",
+    hour: hour,
+    date: date,
+  });
+  return employeeRecord;
+};
+const createTimeOutEvent = (employeeRecord, dateStamp) => {
+  const [date, hour] = getDateAndHour(dateStamp);
+  employeeRecord.timeOutEvents.push({
+    type: "TimeOut",
+    hour: hour,
+    date: date,
+  });
+  return employeeRecord;
+};
+const hoursWorkedOnDate = (employeeRecord, soughtDate) => {
+  const inEvent = employeeRecord.timeInEvents.find(
+    (date) => date.date === soughtDate
+  );
+  const outEvent = employeeRecord.timeOutEvents.find(
+    (date) => date.date === soughtDate
+  );
+  return (outEvent.hour - inEvent.hour) / 100;
+};
+const wagesEarnedOnDate = (employeeRecord, soughtDate) => {
+  return parseFloat(
+    hoursWorkedOnDate(employeeRecord, soughtDate) * employeeRecord.payPerHour
+  );
+};
+const allWagesFor = (employeeRecord) => {
+  const dates = employeeRecord.timeInEvents.map(
+    (timeInEvent) => timeInEvent.date
+  );
+  const payable = dates.reduce((wage, date) => {
+    return wage + wagesEarnedOnDate(employeeRecord, date);
+  }, 0);
+  return payable;
+};
+const calculatePayroll = (employeeRecords) => {
+  return employeeRecords.reduce(
+    (sum, employeeRecord) => sum + allWagesFor(employeeRecord),
+    0
+  );
+};
